851f0d941157358ca90e3a2223eeebad51762630
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-11-22 20:41:58 -0300

    Adding tests for scans.

--- api/app.js
+++ api/app.js
@@ -3,21 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var router = express.Router();
+
+const bodyParser = require('body-parser');
 
 var scanRouter = require("./routes/scan");
 var cors = require('cors');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({origin: 'http://localhost:3000'})); // TODO update with env 
 app.use("/scans", scanRouter);
@@ -38,4 +36,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app, router;
