fd7e7ee2e7c50f1034c53ee39771417fbcac5762
Ramir Mesquita <Ramir_Mesquita@Dellteam.com>
2019-11-23 06:48:28 -0300

    Fixing test args and adding more test scenarios

--- api/__tests__/integration/routes.test.js
+++ api/__tests__/integration/routes.test.js
@@ -27,46 +27,21 @@ const scan = {
    ]
 };
 
-<<<<<<< HEAD
 describe('Scan Endpoints', () => {
-=======
-beforeAll(() => {
-   console.log('Starting TDD development');
-});
-
-afterAll(() => {
-    // server.close(); TODO Fix
-    console.log('server closed');
-});
-
-
-describe('Scan Flow', () => {
->>>>>>> 851f0d941157358ca90e3a2223eeebad51762630
 
    it('should return the scans list', async () => {
       const scanId = 1;
       const res = await request(app).get(`/scans/${scanId}`);
       expect(res.status).toEqual(200);
-<<<<<<< HEAD
       expect(res.body.scans).toHaveLength(0);
    });
 
    it('should save a valid scan and return HTTP 201', () => {   
-=======
-      // expect(res.body).toHaveProperty('post');
-   });
-
-   it('should save a scan and return HTTP 201', () => {   
->>>>>>> 851f0d941157358ca90e3a2223eeebad51762630
       app.post('/scans', async (req, res) => {
          res.send(scan)
             .expect(201, done);  
        });
-<<<<<<< HEAD
        expect(res.body).toHaveProperty('id');
-=======
-       expect(res.body.scans).toHaveLength(0);
->>>>>>> 851f0d941157358ca90e3a2223eeebad51762630
    });
 
    it('should save a scan without findings', async () => {
@@ -81,44 +56,36 @@ describe('Scan Flow', () => {
    it('should respond with status code 400 when an invalid status is sent', async () => {
       const response = await request(app)
          .post('/scans')
-         .send(scan);
-      
-      expect(response.status).toEqual(400);
+         .send(scan)
+         .expect(400);
    });
 
    it('should respond with status code 400 when an invalid timestamp is sent', async () => {
       const response = await request(app)
          .post('/scans')
-<<<<<<< HEAD
-         .send(Object.assign({}, scan, { scanningAt: "Invalid timestamp" }));
-=======
-         .send(scan);
->>>>>>> 851f0d941157358ca90e3a2223eeebad51762630
+         .send(Object.assign({}, scan, { scanningAt: "Invalid timestamp" }))
+         .expect(400);
       
-      expect(response.status).toEqual(400);
    });
 
    it('should fetch a scan', async () => {
       const scanId = 1;
-      const res = await request(app).get(`/scans/${scanId}`);
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.scan).toHaveProperty('id', scanId);
+      const res = await request(app).get(`/scans/${scanId}`)
+         .expect(200)
+         .expect(res.body.scan).toHaveProperty('id', scanId);
     });
 
     it('should respond with status code 404 if a scan is not found', async () => {
       const scanId = 2;
-      const res = await request(app).get(`/scans/${scanId}`);
-      expect(res.statusCode).toEqual(404);
+      const res = await request(app).get(`/scans/${scanId}`)
+         .expect(404);
     });
 
-<<<<<<< HEAD
     it('should respond with status code 400 when an invalid finding record is sent', async () => {
       const response = await request(app)
          .post('/scans')
-         .send(Object.assign({}, scan, { findings: [{id: 'Invalid Finding'}] }));
-      
-      expect(response.status).toEqual(400);
+         .send(Object.assign({}, scan, { findings: [{id: 'Invalid Finding'}] }))
+         .expect(400);
+   
    });
-=======
->>>>>>> 851f0d941157358ca90e3a2223eeebad51762630
 });
